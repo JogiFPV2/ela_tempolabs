ade76f6647f6a64b595219ae13428043b74d2e42
JogiFPV2 <Pawel@bujacze.com>
2025-03-16 23:54:41 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,9 @@
+// Import the dev tools and initialize them
+import { TempoDevtools } from "tempo-devtools"
+TempoDevtools.init();
+
+// Before you render your app
+ReactDOM.render(<App />, document.getElementById("root"));
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
